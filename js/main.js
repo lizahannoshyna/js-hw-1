1f32b84c8b2ef4bdfa483e7a52002416b8cac45d
liza_aaard <91091190+lizahannoshyna@users.noreply.github.com>
2024-10-22 18:55:20 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/* task 1 */
+let age = 16;
+console.log(age);
+
+/* task 2 */
+let name = "Liza";
+console.log(name);
+
+/* task 3 */
+const isStudent = true; /* Boolean */
+
+/* task 4 */
+const quote = 'If not now, when?'; /* Strink рядок */
+
+/* task 5 */
+let myNumber = 19;
+console.log(myNumber + 10);
+
+/* task 6 */
+const myNull = null;
+console.log(null);
+
+/* task 7 */
+let userName = prompt("Введіть своє ім'я:");
+alert("Привіт, " + userName + "!");
+
+/* task 8 */
+let userConfirmation = confirm("Ви впевнені?");
+if (userConfirmation) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
+
+/* task 9 */
+alert("Увага! Дія може бути небезпечною.");
+let actionConfirmation = confirm("Ви впевнені, що хочете продовжити?");
+
+if (actionConfirmation) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
